78e27d022e0d5e1ff2c7a647c2c48298d21bc68d
jiteshjha <jiteshjha96@gmail.com>
2016-02-28 22:26:06 +0530

    Added Pearson Correlation score-based recommendation

--- driver.py
+++ driver.py
@@ -1,3 +1,4 @@
 from recommendations import critics
 from recommendations import sim_distance
-print sim_distance(critics, 'Mick LaSalle', 'Toby')
+from recommendations import sim_pearson
+print sim_pearson(critics, 'Mick LaSalle', 'Toby')
--- recommendations.py
+++ recommendations.py
@@ -35,3 +35,28 @@ def sim_distance(prefs, person1, person2):
             sum_of_squares += pow(prefs[person1][item] - prefs[person2][item], 2)
 
     return 1/(1 + sqrt(sum_of_squares))
+
+# Returns the Pearson correlation coefficient for p1 and p2
+def sim_pearson(prefs,p1,p2):
+    si={}
+    for item in prefs[p1]:
+        if item in prefs[p2]: si[item]=1
+
+    n=len(si)
+
+    if n==0: return 0
+
+    sum1=sum([prefs[p1][it] for it in si])
+    sum2=sum([prefs[p2][it] for it in si])
+
+    sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+    sum2Sq=sum([pow(prefs[p2][it],2) for it in si])
+
+    pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+    # Calculate Pearson score
+    num=pSum-(sum1*sum2/n)
+    den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+    if den==0:
+        return 0
+    r=num/den
+    return r
